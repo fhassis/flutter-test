bd5d4d9bff665e9f821e2b2fb3c62673c677d633
fhassis <fhassis2@gmail.com>
2023-05-01 18:14:22 -0300

    removed projects not useful

--- flutter_candlesticks_chart_test/android/app/src/main/kotlin/com/example/flutter_candlesticks_chart_test/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_candlesticks_chart_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- kchart-test/android/app/src/main/kotlin/com/example/candle_test/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.candle_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
